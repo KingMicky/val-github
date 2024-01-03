9b5decb3968496c165eb6e6b11322718b1cc5702
KingMicky <you@example.com>
2024-01-03 20:06:19 +0100

    Reorganized file structure and added GitHub workflows and templates

--- __init__.py
+++ __init__.py
@@ -1,6 +1,18 @@
-web-api_test/
-|-- __init__.py
-|-- valentine_api.py
-|-- api_test.py
-|-- dockerfile
-|-- README.md
+web-api/
+│
+├── .github/
+│   └── workflows/
+│       ├── python-app.yml
+│       ├── cd.yml
+│       └── ci.yml
+│
+├── templates/
+│   ├── index.html
+│   ├── response.html
+│   └── no_response.html
+│
+├── __init__.py
+├── api_test.py
+├── dockerfile
+├── README.md
+└── valentine_api.py
